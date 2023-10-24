895233c456685b63219a28b9264f55e8d4eb98f2
brainbrian2000 <brainbrian2000@gmail.com>
2023-10-24 20:58:40 +0800

    p3-1

--- p3/proxy_server.py
+++ p3/proxy_server.py
@@ -2,19 +2,23 @@ import socket
 
 # Set the server IP address and port
 # TODO Start
-HOST, PORT =
+HOST, PORT = "127.0.0.1",9999
 # TODO end
 
 # Create a server socket, bind it to the specified IP and port, and start listening
 # TODO Start
-serverSocket =
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind((HOST, PORT))
+serverSocket.listen(1)
 # TODO end
 
 while True:
     print('Ready to serve...')
     # Accept an incoming connection and get the client's address
     # TODO Start
-    client_socket, client_address =
+
+    client_socket, client_address = serverSocket.accept()
+    client_socket.settimeout(10)
     # TODO end
 
     print('Received a connection from:', client_address)
@@ -22,7 +26,7 @@ while True:
     try:
         # Receive request from the client
         # TODO Start
-        request =
+        request = client_socket.recv(1024).decode()
         # TODO end
         print(request)
 
@@ -46,6 +50,10 @@ while True:
             client_socket.send("HTTP/1.1 200 OK\r\n".encode('utf-8'))
             client_socket.send("Content-Type:text/html\r\n\r\n".encode('utf-8'))
             # TODO Start
+            for msg in output_data:
+                client_socket.send(msg.encode())
+            client_socket.close()
+            
             # TODO End
             print('Read from cache')
 
@@ -54,7 +62,7 @@ while True:
             if file_exist == "false":
                 # Create a socket on the proxy server
                 # TODO Start
-                proxy_server_socket =
+                proxy_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 # TODO End
 
                 host_name = filename.replace("www.", "", 1)
@@ -64,6 +72,8 @@ while True:
                     print("Trying to connect to the web server")
                     # Connect the socket to the web server port
                     # TODO Start
+                    proxy_server_socket.connect(("127.0.0.1",10046))
+                    
                     # TODO End
                     print("Connected successfully")
 
@@ -72,7 +82,7 @@ while True:
 
                     # Create the HTTP GET request message to fetch the file from the web server
                     # Write the request to the file-like object
-                    request_message = f"GET {file_path} HTTP/1.0\r\n"
+                    request_message = f"GET {file_path} HTTP/1.1\r\n"
                     print(request_message)
                     file_obj.write(request_message)  # Write the request to the file-like object
                     file_obj.flush()
