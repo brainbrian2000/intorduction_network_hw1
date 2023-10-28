c61c32f12f7e7ab59b14980ee1976373fefba7d4
brainbrian2000 <brainbrian2000@gmail.com>
2023-10-28 18:24:22 +0800

    final

---
+++ b08209023_hw1/p1/socket_server.py
@@ -0,0 +1,139 @@
+import socket
+from datetime import datetime
+
+# Function to calculate the expression
+def calculate_expression(expression):
+    # TODO: Implement this function
+    list=[]
+    if(expression.find('+')>0):
+        for number in (expression.split('+')):
+            list.append(int(number))
+        return list[0]+list[1]    
+    elif(expression.find('-')>0):
+        for number in (expression.split('-')):
+            list.append(int(number))
+        return list[0]-list[1]    
+    elif(expression.find('**')>0):
+        for number in (expression.split('**')):
+            list.append(int(number))
+        return list[0]**list[1]    
+    elif(expression.find('*')>0):
+        for number in (expression.split('*')):
+            list.append(int(number))
+        return list[0]*list[1]    
+    elif(expression.find('/')>0):
+        for number in (expression.split('/')):
+            list.append(int(number))
+        return list[0]/list[1]    
+    elif(expression.find('%')>0):
+        for number in (expression.split('%')):
+            list.append(int(number))
+        return list[0]%list[1]    
+    elif(expression.find('^')>0):
+        for number in (expression.split('^')):
+            list.append(int(number))
+        return list[0]^list[1]    
+    elif(expression.find('>>')>0):
+        for number in (expression.split('>>')):
+            list.append(int(number))
+        return list[0]>>list[1]    
+    elif(expression.find('<<')>0):
+        for number in (expression.split('<<')):
+            list.append(int(number))
+        return list[0]<<list[1]    
+    elif(expression.find('>')>0):
+        for number in (expression.split('>')):
+            list.append(int(number))
+        return list[0]>list[1]    
+    elif(expression.find('<')>0):
+        for number in (expression.split('<')):
+            list.append(int(number))
+        return list[0]<list[1]    
+    
+    return expression+" not found this expression"
+    pass
+
+# Server setup
+# Specify the IP address and port number (Use "127.0.0.1" for localhost on local machine)
+# TODO Start
+HOST, PORT = "127.0.0.1", 10046
+# TODO end
+
+with open('./server_log.txt', 'w') as logFile:
+    # 1. Create a socket
+    # 2. Bind the socket to the address
+    # TODO Start
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.bind((HOST, PORT))
+    # TODO End
+
+    while True:
+        # Listen to a new request with the socket
+        # TODO Start
+        serverSocket.listen(1)
+        # TODO End
+
+        now = datetime.now()
+        print("The Server is running..")
+        logFile.write(now.strftime("%H:%M:%S ") + "The Server is running..\n")
+        logFile.flush()
+
+
+        # Accept a new request and admit the connection
+        # TODO Start
+        client, address = serverSocket.accept()
+        # TODO End
+
+        client.settimeout(15)
+        print(str(address) + " connected")
+        now = datetime.now()
+        logFile.write(now.strftime("%H:%M:%S ") + "connected " + str(address) + '\n')
+        logFile.flush()
+
+        try:
+            while True:
+                client.send(b"Please input a question for calculation")
+
+                # Recieve the data from the client
+                # TODO Start
+                
+                question = client.recv(1024).decode()
+                # TODO End
+
+                now = datetime.now()
+                logFile.write(now.strftime("%H:%M:%S ") + question + '\n')
+                logFile.flush()
+
+                # TODO: Call the calculate_expression function here
+                try:
+                    ans = float(calculate_expression(question))
+                except:
+                    ans = calculate_expression(question)
+                # print ("ans = " + str(ans))
+                # Ask if the client want to terminate the process
+                message = f"{ans}\nDo you wish to continue? (Y/N)"
+
+
+                # Send the answer back to the client
+                # TODO Start
+                client.send((message).encode())
+                ans= client.recv(1024).decode()
+                # TODO End
+
+                # Terminate the process or continue
+                if ans.lower() != 'y':
+                    client.close()
+                    break
+        except ConnectionResetError:
+            print("Connection reset by peer")
+            logFile.write("Connection reset by peer\n")
+            logFile.flush()
+        except Exception as e:
+            print("An error occurred:", e)
+            logFile.write(f"An error occurred: {e}\n")
+            logFile.flush()
+
+        # print("testing1")
+        client.close()
+        # break
+logFile.close()
---
+++ b08209023_hw1/p2/web_server.py
@@ -0,0 +1,133 @@
+import socket
+import sys
+
+# Server setup
+# Specify the IP address and port number (Use "127.0.0.1" for localhost on local machine)
+# TODO Start
+# HOST, PORT = "127.0.0.1",10045
+HOST, PORT = "127.0.0.1",10046
+
+# TODO end
+
+
+# 1. Create a socket
+# 2. Bind the socket to the address
+# TODO Start
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# TODO End
+
+# Listen for incoming connections (maximum of 1 connection in the queue)
+# TODO Start
+
+serverSocket.bind((HOST,PORT))
+serverSocket.listen(1)
+# TODO End
+
+# Start an infinite loop to handle incoming client requests
+while True:
+    print('Ready to serve...')
+
+    # Accept an incoming connection and get the client's address
+    # TODO Start
+    connectionSocket, address = serverSocket.accept()
+    connectionSocket.settimeout(10)
+    # TODO End
+    print(str(address) + " connected")
+
+    try:
+        # Receive and decode the client's request
+        # TODO Start
+        message = connectionSocket.recv(1024).decode()
+        print("MESSAGE:")
+        print(message)
+        print("")
+        # TODO End
+
+        # If the message is empty, set it to a default value
+        if message == "":
+            message = "/ /"
+            filename = ""
+        else:
+        # Print the client's request message
+            print(f"client's request message: \n {message}")
+
+            # Extract the filename from the client's request
+            # TODO Start
+            message_arr_1 = message.split(" ")
+            
+            if message_arr_1[0] == "GET":
+                message_arr = message_arr_1[1].split("/")
+                filename = "."+message_arr_1[1]
+                # print(message_arr)
+                if(message_arr[1]=="index.html"):  # or message_arr[1]=="/index" or message_arr[1]==""):
+                    filename = "index.html"
+                elif(message_arr[1]==""):
+                    connectionSocket.send("HTTP/1.1 404 Not Found\n\n<h1>404 Not Found</h1>".encode())
+                    connectionSocket.close()
+                    continue
+                elif(message_arr[1]=="test"):
+                    raise NameError(message_arr[2])
+                else:
+                    # filename = ""
+                    pass
+            else:
+                pass
+        # TODO End
+        print(f"Extract the filename: {filename}")
+
+        # Open the requested file
+        # Read the file's content and store it in a list of lines
+        f = open(filename)
+        outputdata = f.readlines()
+        # 1. Send an HTTP response header to the client
+        # 2. Send the content of the requested file to the client line by line
+        # 3. Close the connection to the client
+        # TODO Start
+        connectionSocket.send(("HTTP/1.1 200 OK\n\n").encode())
+        http_inside = ""
+        for msg in outputdata:
+            # http_inside+=msg
+            connectionSocket.sendall((msg).encode())
+        # print(http_inside)
+        # connectionSocket.sendall(http_inside.encode())
+        connectionSocket.close()
+        f.close()
+        # TODO End
+
+    except NameError as e:
+        # If the requested file is not found, send a 404 Not Found response
+        # TODO Start
+        
+        Errorargs= e.args
+        # print(type(Errorargs))
+        print((Errorargs))
+        
+        arg=Errorargs[0]
+        print(f"in NameError: {arg}")
+        if(arg=="404"):
+            connectionSocket.send("HTTP/1.1 404 Not Found\n\n<h1>404 Not Found</h1>".encode())
+            connectionSocket.close()
+        else:
+            try:
+                arg = int(arg)
+                print(f"in Error: {arg}")
+                connectionSocket.send(f"HTTP/1.1 {arg}\n\n".encode())
+                connectionSocket.send(f"<h1>Get Error Code(Testing): {arg}</h1>".encode())
+            except:
+                connectionSocket.send("HTTP/1.1 404 Not Found\n\n<h1>404 Not Found</h1>".encode())
+                connectionSocket.close()
+                
+        connectionSocket.close()
+            
+        pass
+    except (OSError) as e:
+        connectionSocket.send("HTTP/1.1 404 Not Found\n\n<h1>404 Not Found</h1>".encode())
+        connectionSocket.close()
+    except Exception as e:
+        connectionSocket.close()
+        print("Exception:")
+        print(e)
+        # TODO End
+    
+serverSocket.close()
